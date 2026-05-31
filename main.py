54c2ad6eed81d976df3ac550633e7871a6c7676a
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 17:17:45 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class loggerEeleteWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = loggerEeleteWorker()
+    print("Worker engine initialized.")
